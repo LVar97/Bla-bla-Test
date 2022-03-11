c0cf24e5f4ddaea6bd87c8bd3ad4ccd6c37df617
Varvara Karelina <letkovaa@mail.ru>
2022-03-11 23:16:21 +0300

    fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,11 +3,31 @@ import "./Footer.scss";
 
 
 export const Footer: React.FC = () => {
+
+	const handleButtonUp = (event:  React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const menuLink = event.target as HTMLButtonElement;
+    
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock?.getBoundingClientRect().top;
+      
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      })
+    }
+  }
+
 	return (
 		<footer className="footer">
 			<div className="container">
 				<p className="footer__copyright">© Copyright 2016 Bla Bla Studio  |  One Page Flat Template</p>
-				<button className="footer__button fa-solid fa-angle-up"></button>
+				<button 
+				data-goto=".header"
+				className="footer__button fa-solid fa-angle-up"
+				onClick={(event) => handleButtonUp(event)}
+				></button>
 			</div>
 		</footer>
 	)
